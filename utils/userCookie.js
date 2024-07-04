13c84ea10cbe2cc27b85664a07673eed4bdb830b
Shakshi Pandey <shakshipandey560@gmail.com>
2024-07-04 21:37:31 +0530

    changes in some issue

--- utils/userCookie.js
+++ utils/userCookie.js
@@ -7,7 +7,7 @@ export const userCookie = (res,user,message, statusCode= 200 )=>{
     res.status(statusCode).cookie("token",token,{
         httpOnly: true,
         maxAge: 15*60*1000,
-        sameSite: process.env.NODE_ENV === "Development"? lax:none,
+        sameSite: process.env.NODE_ENV === "Development"? "lax": "none",
         secure: process.env.NODE_ENV==="Development"?false:true
     }).json({
         success: true, 
